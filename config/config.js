c66ac6092054b00c289decf0680ec5165c1df37b
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-05-05 14:25:56 -0400

    Configure Sequelize for database connection

---
+++ config/config.js
@@ -0,0 +1,16 @@
+// config/config.js
+require('dotenv').config();  // Ensures we can use environment variables
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql'
+  }
+};
